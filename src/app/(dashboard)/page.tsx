db8b76b7c0c9bb8deabb02d51a041bde35673479
Manu Sai <v.s.manu2004@gmail.com>
2025-12-19 08:47:00 +0530

    08- trpc setup

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,16 @@
 import HomeView from "@/modules/home/ui/views/home-view";
+import { caller } from "@/trpc/server";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
 
-const Page = () => {
+const Page = async () => {
+  const data = await caller.hello({ text: "Manu Server" });
+  const session =await auth.api.getSession({
+    headers:await headers(),
+  })
+  return (
+    <p>{data.greeting}</p>
+  )
   return <HomeView />;
 };
 
